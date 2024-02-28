0948392377dc198bbe1e1e2a51ccd5c9aa01798a
Santiago Bruno <santiago.p.bruno@gmail.com>
2024-02-28 17:29:33 +0100

    commit

--- scaricabile.js
+++ scaricabile.js
@@ -1,17 +1,4 @@
 /*
-
-
-Putroppo non ce l'ho fatta a finire il compito, ho avuto difficoltà ovviamente ma ho avuto anche poco tempo ed è solo
-responsabilità mia. Mi dispiace e cercherò di fare di meglio nei prossimi giorni perché capisco che questo modulo è
-fondamentale per tutto il corso.
-Comunque nei prossimi giorni continuerò a lavorarci per finirlo anche fuori della data prevista.
-
-
-
-
-
-
-
 PARTE 1: 
 Oggi analizzeremo un problema molto comune: realizzare algoritmi di ricerca.
 Il tuo compito è creare una funzione che cercherà per posizione lavorativa E posizione geografica. Questi due valori verranno passati come parametri
@@ -38,154 +25,146 @@ REQUISITI:
 - la tua ricerca deve essere "case insensitive" (non deve essere influenzata da lettere maiuscole o minuscole nelle parole cercate). Questo e' possibile trasformando tutto in lettere minuscole con .toLowerCase()
 
 
-
 PARTE 2: 
 Nella pagina HTML, inserisci 2 input di tipo testo (uno per la location e uno per il titolo lavorativo, ricordati di diversificarli con un id) e un bottone con valore “cerca”
 
 Al click del bottone, il codice deve raccogliere i valori dei due input e darli in pasto alla funzione che hai creato nella parte 1. 
 
 Dopo aver raccolto ed elaborato i dati, e’ il momento di mostrare i risultati sulla pagina: 
-Puoi scegliere tu se utilizzare un semplice ul / li oppure una tabella 
-Vai passo per passo e usa molti console.log per capire eventualmente dove sbagli
-SUGGERIMENTO: ti servira’ un ciclo for!
+    Puoi scegliere tu se utilizzare un semplice ul / li oppure una tabella 
+    Vai passo per passo e usa molti console.log per capire eventualmente dove sbagli
+    SUGGERIMENTO: ti servira’ un ciclo for!
 
 */
+
 // NON MODIFICARE QUESTO ARRAY!
 const jobs = [
-      { title: "Marketing Intern", location: "US, NY, New York" },
-      {
-        title: "Customer Service - Cloud Video Production",
-        location: "NZ, Auckland",
-      },
-      {
-        title: "Commissioning Machinery Assistant (CMA)",
-        location: "US, IA, Wever",
-      },
-      {
-        title: "Account Executive - Washington DC",
-        location: "US, DC, Washington",
-      },
-      { title: "Bill Review Manager", location: "US, FL, Fort Worth" },
-      { title: "Accounting Clerk", location: "US, MD," },
-      { title: "Head of Content (m/f)", location: "DE, BE, Berlin" },
-      {
-        title: "Lead Guest Service Specialist",
-        location: "US, CA, San Francisco",
-      },
-      { title: "HP BSM SME", location: "US, FL, Pensacola" },
-      {
-        title: "Customer Service Associate - Part Time",
-        location: "US, AZ, Phoenix",
-      },
-      {
-        title: "ASP.net Developer Job opportunity at United States,New Jersey",
-        location: "US, NJ, Jersey City",
-      },
-      {
-        title: "Talent Sourcer (6 months fixed-term contract)",
-        location: "GB, LND, London",
-      },
-      {
-        title: "Applications Developer, Digital",
-        location: "US, CT, Stamford",
-      },
-      { title: "Installers", location: "US, FL, Orlando" },
-      { title: "Account Executive - Sydney", location: "AU, NSW, Sydney" },
-      {
-        title: "VP of Sales - Vault Dragon",
-        location: "SG, 01, Singapore",
-      },
-      { title: "Hands-On QA Leader", location: "IL, Tel Aviv, Israel" },
-      {
-        title: "Southend-on-Sea Traineeships Under NAS 16-18 Year Olds Only",
-        location: "GB, SOS, Southend-on-Sea",
-      },
-      { title: "Visual Designer", location: "US, NY, New York" },
-      {
-        title: "Process Controls Engineer - DCS PLC MS Office - PA",
-        location: "US, PA, USA Northeast",
-      },
-      { title: "Marketing Assistant", location: "US, TX, Austin" },
-      { title: "Front End Developer", location: "NZ, N, Auckland" },
-      { title: "Engagement Manager", location: "AE," },
-      {
-        title: "Vice President, Sales and Sponsorship (Businessfriend.com)",
-        location: "US, CA, Carlsbad",
-      },
-      { title: "Customer Service", location: "GB, LND, London" },
-      { title: "H1B SPONSOR FOR L1/L2/OPT", location: "US, NY, New York" },
-      { title: "Marketing Exec", location: "SG," },
-      {
-        title: "HAAD/DHA Licensed Doctors Opening in UAE",
-        location: "AE, AZ, Abudhabi",
-      },
-      {
-        title: "Talent Management Process Manager",
-        location: "US, MO, St. Louis",
-      },
-      { title: "Customer Service Associate", location: "CA, ON, Toronto" },
-      {
-        title: "Customer Service Technical Specialist",
-        location: "US, MA, Waltham",
-      },
-      { title: "Software Applications Specialist", location: "US, KS," },
-      { title: "Craftsman Associate", location: "US, WA, Everett" },
-      { title: "Completion Engineer", location: "US, CA, San Ramon" },
-      { title: "I Want To Work At Karmarama", location: "GB, LND," },
-      {
-        title: "English Teacher Abroad",
-        location: "US, NY, Saint Bonaventure",
-      },
-    ]
+  { title: "Marketing Intern", location: "US, NY, New York" },
+  {
+    title: "Customer Service - Cloud Video Production",
+    location: "NZ, Auckland",
+  },
+  {
+    title: "Commissioning Machinery Assistant (CMA)",
+    location: "US, IA, Wever",
+  },
+  {
+    title: "Account Executive - Washington DC",
+    location: "US, DC, Washington",
+  },
+  { title: "Bill Review Manager", location: "US, FL, Fort Worth" },
+  { title: "Accounting Clerk", location: "US, MD," },
+  { title: "Head of Content (m/f)", location: "DE, BE, Berlin" },
+  {
+    title: "Lead Guest Service Specialist",
+    location: "US, CA, San Francisco",
+  },
+  { title: "HP BSM SME", location: "US, FL, Pensacola" },
+  {
+    title: "Customer Service Associate - Part Time",
+    location: "US, AZ, Phoenix",
+  },
+  {
+    title: "ASP.net Developer Job opportunity at United States,New Jersey",
+    location: "US, NJ, Jersey City",
+  },
+  {
+    title: "Talent Sourcer (6 months fixed-term contract)",
+    location: "GB, LND, London",
+  },
+  {
+    title: "Applications Developer, Digital",
+    location: "US, CT, Stamford",
+  },
+  { title: "Installers", location: "US, FL, Orlando" },
+  { title: "Account Executive - Sydney", location: "AU, NSW, Sydney" },
+  {
+    title: "VP of Sales - Vault Dragon",
+    location: "SG, 01, Singapore",
+  },
+  { title: "Hands-On QA Leader", location: "IL, Tel Aviv, Israel" },
+  {
+    title: "Southend-on-Sea Traineeships Under NAS 16-18 Year Olds Only",
+    location: "GB, SOS, Southend-on-Sea",
+  },
+  { title: "Visual Designer", location: "US, NY, New York" },
+  {
+    title: "Process Controls Engineer - DCS PLC MS Office - PA",
+    location: "US, PA, USA Northeast",
+  },
+  { title: "Marketing Assistant", location: "US, TX, Austin" },
+  { title: "Front End Developer", location: "NZ, N, Auckland" },
+  { title: "Engagement Manager", location: "AE," },
+  {
+    title: "Vice President, Sales and Sponsorship (Businessfriend.com)",
+    location: "US, CA, Carlsbad",
+  },
+  { title: "Customer Service", location: "GB, LND, London" },
+  { title: "H1B SPONSOR FOR L1/L2/OPT", location: "US, NY, New York" },
+  { title: "Marketing Exec", location: "SG," },
+  {
+    title: "HAAD/DHA Licensed Doctors Opening in UAE",
+    location: "AE, AZ, Abudhabi",
+  },
+  {
+    title: "Talent Management Process Manager",
+    location: "US, MO, St. Louis",
+  },
+  { title: "Customer Service Associate", location: "CA, ON, Toronto" },
+  {
+    title: "Customer Service Technical Specialist",
+    location: "US, MA, Waltham",
+  },
+  { title: "Software Applications Specialist", location: "US, KS," },
+  { title: "Craftsman Associate", location: "US, WA, Everett" },
+  { title: "Completion Engineer", location: "US, CA, San Ramon" },
+  { title: "I Want To Work At Karmarama", location: "GB, LND," },
+  {
+    title: "English Teacher Abroad",
+    location: "US, NY, Saint Bonaventure",
+  },
+]
 
-function findJob(array, title = "", location = "") {
-  let result = [];
-  let count = [];
-  
-  for (let i = 0; i < array.length; i++) {
-    let obj = array[i];
-    let titleMatch = obj.title.toLowerCase().includes(title.toLowerCase());
-    let locationMatch = obj.location.toLowerCase().includes(location.toLowerCase());
+const findJob = (queryLocation, queryTitle) => {
+  let globalResults = {
+    result: [],
+    count: 0,
+  }
+  for (let i = 0; i < jobs.length; i++) {
+    const obj = jobs[i]
+    // if(obj.location.toLowerCase().includes(queryLocation.toLowerCase()) && lavoro.title.toLowerCase().includes(queryTitle.toLowerCase())) {
+    //     globalResults.results.push(obj)
+    //     globalResults.count++
+    // }
+    let locationActiveJob = obj.location.toLowerCase()
+    let titleActiveJob = obj.title.toLowerCase()
+    let queryTitleMin = queryTitle.toLowerCase()
+    let queryLocationMin = queryLocation.toLowerCase()
     
-    if (titleMatch && locationMatch) {
-      result.push(obj);
-    }
-    
-    if (titleMatch || locationMatch) {
-      count.push(1);
+    if (
+      locationActiveJob.includes(queryLocationMin) &&
+      titleActiveJob.includes(queryTitleMin)
+    ) {
+      globalResults.result.push(obj)
+      globalResults.count++
     }
   }
-  
-  return {
-    result : result,
-    count : result.length
-  };
-
+  return globalResults
 }
 
 
-
-
-
-let foundPosition = ""; // ----> se riempio questa stringa con un valore a trovare, l'algoritmo funziona
-let foundLocation = ""; // ----> se riempio questa stringa con un valore a trovare, l'algoritmo funziona anche qua
-
-// let foundPosition = document.getElementById("position-text").value; ----> invece qua non mi prende il valore del input text
-
-// let foundLocation = document.getElementById("location-text").value; ----> stessa cosa qua
-
-let foundResult = findJob(jobs, foundPosition, foundLocation);
-console.log(foundResult);
-
-
-
-
-
-document.getElementById("my-button").onclick = function () {findJob()};
-
-
-// let listPosition = document.getElementById("position-id");
-// listPosition.innerHTML = JSON.stringify(foundResult.result);
-  
-
-
+const finalResult= () => {
+  let titleValue = document.getElementById("title").value
+  let locationValue = document.getElementById("location").value
+  let printResultArea = document.getElementById("print-result")
+  const res = findJob(locationValue, titleValue)
+  for (let i = 0; i < res.result.length; i++) {
+    const obj = res.result[i];
+    
+    let printResult = document.createElement("li")
+    printResult.innerText = `Position Found: ${obj.title} - Location: ${obj.location}`
+    printResultArea.appendChild(printResult);
+    titleValue = "";
+    locationValue = "";
+  }
+}
